bf6f83899241abb39d50c25d9eecd8cb2c0aa172
Dcastroe10 <dcastroeli@gmail.com>
2023-10-02 22:44:15 -0600

    Update FSM
    Falta agregar la lógica que verifica las condiciones de victoria o derrota

--- Laboratorio 4/FSM.sv
+++ Laboratorio 4/FSM.sv
@@ -1,19 +1,23 @@
 module FSM (
-	input logic entrada,
+	input logic mov,
+	input logic sel_flag,
+	input logic sel,
 	input logic clk,
 	input logic rst,
-	output logic test
+	output logic result
 );
 
  logic [2:0] state, next_state;
- logic test = 1;
+ //logic test = 1;
+ logic bomb = 0;
+ logic win = 0;
  
 
 
 
 // Actual state logic
   always_ff @(posedge clk or posedge rst)
-	if(rst) state = 2'b000;
+	if(rst) state = 3'b000;
 	else
 		state = next_state;
 
@@ -21,47 +25,53 @@ module FSM (
   always_comb
   begin
     case(state)
-		 // Estado 0
-        2'b000: next_state = 2'b000;
-		  
+			// Estado 0
+        3'b000: if (mov) next_state = 3'b111;
+               else if (sel_flag) next_state = 3'b110;
+               else if (sel) next_state = 3'b001;
+               else next_state = 3'b000;
+
         // Estado 1
-        2'b001: next_state = 2'b001;
-		  
+        3'b001: if (bomb) next_state = 3'b011;  //Revisar si hay bomba o no
+					else if(~bomb) next_state = 3'b010;
+					else next_state = 3'b001;
+					
         // Estado 2
-        2'b010: next_state = 2'b010;
+        3'b010: if (win) next_state = 3'b101; //Revisar si se ganó o no
+					else if(~win) next_state = 3'b100;
+					else next_state = 3'b010;
 		  
         // Estado 3
-        2'b011: next_state = 2'b011;
+        3'b011: next_state = 3'b011; //Perdió y solo se sale con rst
 		  
         // Estado 4
-        2'b100: next_state = 2'b100;
+        3'b100: next_state = 3'b000; //Estado para continuar jugando cuando no hay victoria
 		  
         // Estado 5
-        2'b101: next_state = 2'b101;
+        3'b101: next_state = 3'b101; //Ganó, solo se sale con rst
 		  
         // Estado 6
-        2'b110: next_state = 2'b110;
+        3'b110: next_state = 3'b000;
 		  
         // Estado 7
-        2'b111: next_state = 2'b111;
+        3'b111: next_state = 3'b000;
 		  
-        default: next_state = 2'b000;
+        default: next_state = 3'b000;
 		  
     endcase
 	
   end
   
-  //Output Logic (To be able to use if and else
+  //Output Logic (To be able to use if and else)
   always @(next_state) begin
-
-		if(t0)begin
+		/*
+		if(test)begin
 		test=0;
 		$display("prueba");
 		end else begin
-		test=1;
 		$display("OMG SIRVE EL ELSE");
 		end
-  
+  */
   
   end
   
